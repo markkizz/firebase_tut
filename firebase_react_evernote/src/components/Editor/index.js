75989c16308612a7dc691a6e3911c075ec734d70
markkizz <mark.pattanapara@gmail.com>
2020-02-20 21:49:04 +0700

    Setup components

---
+++ firebase_react_evernote/src/components/Editor/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import ReactQuill from 'react-quill';
+import debounce from '../../helpers';
+import BorderColorIcon from '@material-ui/icons/BorderColor';
+import { withStyles } from '@material-ui/core/styles';
+import styles from './style'
+
+export class Editor extends Component {
+  render() {
+    return (
+      <>
+
+      </>
+    )
+  }
+}
+
+export default withStyles(styles)(Editor)
---
+++ firebase_react_evernote/src/components/Sidebar/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { withStyles } from '@material-ui/core/styles';
+import styles from './styles';
+import List from '@material-ui/core/List';
+import { Divider, Button } from '@material-ui/core';
+import SidebarItem from '../SidebarItem';
+
+export class Sidebar extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default Sidebar
